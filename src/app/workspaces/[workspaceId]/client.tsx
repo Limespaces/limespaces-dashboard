3839d5b6311af00893193bd01a1f91879daaadbd
Martin Petr <martinpetrnp@gmail.com>
2026-06-20 09:30:48 +0200

    Add container power actions

---
+++ src/app/workspaces/[workspaceId]/client.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { IServerActionDispatcher } from "@/api/actions";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/ui/spinner";
+import { useServerAction } from "@/hooks/useServerAction";
+import { EWorkspaceContainerState } from "@limespaces/shared";
+import { PlayIcon, PowerIcon, StopIcon } from "@phosphor-icons/react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+
+interface IPowerWorkspaceButtonProps {
+  powerWorkspace: IServerActionDispatcher<
+    {
+      workspaceId: string;
+      action: "start" | "stop";
+    },
+    {}
+  >;
+  workspaceId: string;
+  currentState?: EWorkspaceContainerState;
+}
+
+export function PowerWorkspaceButton(props: IPowerWorkspaceButtonProps) {
+  const router = useRouter();
+
+  const { isLoading, dispatch } = useServerAction(props.powerWorkspace, {
+    onError: (error) => toast.error(error.message),
+    onSuccess: () => router.refresh(),
+  });
+
+  return (
+    <div className="flex flex-row gap-1">
+      <Button
+        onClick={() =>
+          dispatch({ workspaceId: props.workspaceId, action: "start" })
+        }
+        disabled={
+          isLoading || props.currentState != EWorkspaceContainerState.Stopped
+        }
+        className="h-8 w-8"
+      >
+        {isLoading ? <Spinner /> : <PlayIcon />}
+      </Button>
+      <Button
+        onClick={() =>
+          dispatch({ workspaceId: props.workspaceId, action: "stop" })
+        }
+        disabled={
+          isLoading || props.currentState != EWorkspaceContainerState.Running
+        }
+        className="h-8 w-8"
+      >
+        {isLoading ? <Spinner /> : <StopIcon />}
+      </Button>
+    </div>
+  );
+}
---
+++ src/app/workspaces/[workspaceId]/page.tsx
@@ -0,0 +1,75 @@
+import { createServerAction } from "@/api/actions";
+import { API, ApiVersion } from "@/api/API";
+import { AppShell } from "@/components/app/appshell";
+import { PowerWorkspaceButton } from "./client";
+import { ApiErrorScreen } from "@/components/screens/error";
+import {
+  Dto_Workspace_Get,
+  EWorkspaceContainerState,
+} from "@limespaces/shared";
+import { WorkspaceContainerStateTranslation } from "@/lang/enum-translations";
+
+interface IPageProps {
+  params: Promise<{
+    workspaceId: string;
+  }>;
+}
+
+export default async function Page(props: IPageProps) {
+  const { workspaceId } = await props.params;
+
+  const workspace = await API.get<Dto_Workspace_Get>(
+    ApiVersion.v1,
+    `/workspace/${workspaceId}`,
+  );
+
+  const powerWorkspace = createServerAction<
+    {
+      workspaceId: string;
+      action: "start" | "stop";
+    },
+    {}
+  >(
+    async (
+      data: {
+        workspaceId: string;
+        action: "start" | "stop";
+      },
+      resolve,
+    ) => {
+      "use server";
+
+      const result = await API.post<{}, {}>(
+        ApiVersion.v1,
+        `/workspace/${data.workspaceId}/power/${data.action}`,
+        {},
+      );
+
+      if (result.isOk) resolve.ok(result.data);
+      else resolve.error(new Error(result.data.message));
+    },
+  );
+
+  if (!workspace.isOk) return <ApiErrorScreen error={workspace.data} />;
+
+  return (
+    <AppShell>
+      <div className="p-16 flex flex-col gap-4">
+        <p>{workspace.data.name}</p>
+        <p>
+          {workspace.data.workspaceContainer
+            ? WorkspaceContainerStateTranslation[
+                workspace.data.workspaceContainer.state
+              ]
+            : "Unknown"}
+        </p>
+
+        <PowerWorkspaceButton
+          powerWorkspace={powerWorkspace}
+          workspaceId={workspaceId}
+          currentState={workspace.data.workspaceContainer?.state}
+        />
+      </div>
+    </AppShell>
+  );
+}
